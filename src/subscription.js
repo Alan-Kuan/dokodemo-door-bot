a195b525e7bd8d4648a1fa3b95558fae72bdaf4b
Alan-Kuan <kuanhaoan@gmail.com>
2021-06-16 00:31:09 +0800

    Change: use 'serverless-postgres' to replace 'google-spreadsheet'.

--- src/subscription.js
+++ src/subscription.js
@@ -1,49 +1,40 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+const pg = require('serverless-postgres');
 
-const doc_id = process.env.GDOC_ID;
-const sheet_id = '0';
-const creds = require('../credentials.json');
-const doc = new GoogleSpreadsheet(doc_id);
+const db = new pg({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DB,
+    password: process.env.PG_PASSWD,
+    port: 5432,
+    debug: true,
+    delayMs: 3000
+});
+
 async function subscribe(chat_id) {
-    await doc.useServiceAccountAuth(creds);
-    await doc.loadInfo();
-    const sheet = doc.sheetsById[sheet_id];
-    const rows = await sheet.getRows();
-    for(let row of rows) {
-        // already subscribed
-        if(row._rawData[0] === chat_id) {
-            return false;
-        }
+    await db.connect();
+    let res = await db.query(`SELECT chat_id FROM subscribers WHERE chat_id = ${chat_id}`);
+    // have subscribed
+    if(res.rowCount > 0) {
+        await db.end();
+        return false;
     }
-    await sheet.addRow([`'${chat_id}`]);
+    await db.query(`INSERT INTO subscribers VALUES(${chat_id})`);
+    await db.end();
     return true;
 }
 
 async function unsubscribe(chat_id) {
-    await doc.useServiceAccountAuth(creds);
-    await doc.loadInfo();
-    const sheet = doc.sheetsById[sheet_id];
-    const rows = await sheet.getRows();
-    for(let row of rows) {
-        if(row._rawData[0] === chat_id) {
-            await row.delete();
-            return true;
-        }
-    }
-    // have not subscribed
-    return false;
+    await db.connect();
+    let res = await db.query(`DELETE FROM subscribers WHERE chat_id = ${chat_id}`);
+    await db.end();
+    return res.rowCount > 0;
 }
 
 async function getSubscribers() {
-    await doc.useServiceAccountAuth(creds);
-    await doc.loadInfo();
-    const sheet = doc.sheetsById[sheet_id];
-    const rows = await sheet.getRows();
-    let subscribers = [];
-    for(let row of rows) {
-        subscribers.push(row._rawData[0]);
-    }
-    return subscribers;
+    await db.connect();
+    let res = await db.query(`SELECT chat_id FROM subscribers`);
+    await db.end();
+    return res.rows.map(e => e.chat_id);
 }
 
 module.exports = { subscribe, unsubscribe, getSubscribers };
