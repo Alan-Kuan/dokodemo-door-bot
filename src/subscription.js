b378d41263426b26317f2b2b52361b8e4485ead5
Alan-Kuan <kuanhaoan@gmail.com>
2021-07-21 22:11:34 +0800

    Change: use clean() instead of end() when finishing a query.

--- src/subscription.js
+++ src/subscription.js
@@ -1,19 +1,19 @@
 const pg = require('serverless-postgres');
 
 const db = new pg({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DB,
-    password: process.env.PG_PASSWD,
-    port: 5432,
-    debug: true,
-    delayMs: 3000
-});
+        host: process.env.PG_HOST,
+        database: process.env.PG_DB,
+        user: process.env.PG_USER,
+        password: process.env.PG_PASSWD,
+        port: 5432,
+        debug: true,
+        delayMs: 3000
+    }, maxConnections=5);
 
 async function haveSubscribed(user_id) {
     await db.connect();
     let res = await db.query(`SELECT user_id FROM subscribers WHERE user_id = ${user_id}`);
-    await db.end();
+    await db.clean();
     return res.rowCount > 0;
 }
 
@@ -22,25 +22,25 @@ async function subscribe(user_id) {
     let res = await db.query(`SELECT user_id FROM subscribers WHERE user_id = ${user_id}`);
     // have subscribed
     if(res.rowCount > 0) {
-        await db.end();
+        await db.clean();
         return false;
     }
     await db.query(`INSERT INTO subscribers VALUES(${user_id})`);
-    await db.end();
+    await db.clean();
     return true;
 }
 
 async function unsubscribe(user_id) {
     await db.connect();
     let res = await db.query(`DELETE FROM subscribers WHERE user_id = ${user_id}`);
-    await db.end();
+    await db.clean();
     return res.rowCount > 0;
 }
 
 async function getSubscribers(src) {
     await db.connect();
     let res = await db.query(`SELECT user_id FROM subscribers NATURAL JOIN user_preference WHERE img_source = '${src}'`);
-    await db.end();
+    await db.clean();
     return res.rows.map(e => e.user_id);
 }
 
